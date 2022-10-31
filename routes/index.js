2e34b740adf9dc308ab2edbb4573cf595ebd9402
Krishnadev P Melevila <krishnadev@kali>
2022-10-31 21:05:05 +0530

    Code Optmisation

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,40 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "PInG-T" });
 });
 router.post("/", (req, res) => {
-  axios
-    .get(
-      process.env.VERIFICATION_API +
-        req.body.upiId +
-        "@paytm"
-    )
-    .then((response) => {
 
-      if (response.data.data.status == "TXN_FAILED") {
-        axios
-          .get(
-            process.env.VERIFICATION_API +
-              req.body.upiId +
-              "@ybl"
-          )
-          .then((response) => {
-            console.log(response.data);
-            res.send(response.data);
-          })
-          .catch((err) => {
-            res.status(400).send(err.response.data);
-          });
-      } else {
+  axios.all([
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@paytm"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@ybl"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@okicici"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@okaxis"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@okhdfcbank"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@ibl"),
+    axios.get(process.env.VERIFICATION_API + req.body.upiId + "@axl"),
 
-        res.send(response.data);
-      }
-    })
-    .catch((err) => {
-      res.status(400).send(err.response.data);
-    });
+
+  ]).then(axios.spread((...responses) => {
+    const responseOne = responses[0]
+    const responseTwo = responses[1]
+    const responseThree = responses[2]
+    const responseFour = responses[3]
+    const responseFive = responses[4]
+    const responseSix = responses[5]
+    const responseSeven = responses[6]
+    response=[responseOne.data,responseTwo.data,responseThree.data,responseFour.data,responseFive.data,responseSix.data,responseSeven.data]
+
+    for(i=0;i<response.length;i++){
+      if(response[i].data.status=="TXN_SUCCESS"){
+        res.send(response[i].data);
+        break;
+      }else if(response[i].data.status=="TXN_FAILED"){
+        if(i==response.length-1){
+        res.send(response[i].data);
+        }}
+    }
+    
+  })).catch(err => {
+    res.status(400).send(err.response.data);
+  })
  
   
       
